ce6cca372ce4ff54fb24149a4914a746df0af3db
MatthiasWiesmann21 <matthias@Matthiass-MacBook-Pro.local>
2023-10-28 19:58:11 +0200

    hopefully yet

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
@@ -41,9 +41,8 @@ export const ChapterVideoForm = ({
         <iframe 
           src={`https://player.vimeo.com/video/${videoId}`} 
           style={{ position: "absolute", top: "0", left: "0", width: "100%", height: "100%" }} 
-          frameborder="0" 
           allow="autoplay; fullscreen" 
-          allowfullscreen>
+          >
         </iframe>
       </div>
     );
