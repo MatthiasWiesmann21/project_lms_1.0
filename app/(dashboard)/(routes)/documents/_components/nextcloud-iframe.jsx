46d78ff5d034edac299a0dc28eec29ef973d7920
MatthiasWiesmann21 <matthias@Matthiass-MacBook-Pro.local>
2023-11-30 16:21:48 +0100

    idk

--- app/(dashboard)/(routes)/documents/_components/nextcloud-iframe.jsx
+++ app/(dashboard)/(routes)/documents/_components/nextcloud-iframe.jsx
@@ -6,13 +6,9 @@ const NextcloudIframe = ({ src, width, height }) => {
       src={src}
       width={width || "100%"}
       height={height || "600px"}
-      frameBorder="0"
       allowFullScreen
-      sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
       style={{ border: "none", overflow: "hidden" }}
-      loading="lazy"
     >
-      Ihr Browser unterstützt keine iframes.
     </iframe>
   );
 };
--- app/(dashboard)/(routes)/documents/page.tsx
+++ app/(dashboard)/(routes)/documents/page.tsx
@@ -4,7 +4,7 @@ import NextcloudIframe from './_components/nextcloud-iframe';
 const DocumentsPage = () => {
     return (
         <div>
-          <NextcloudIframe src="https://nextcloud.matthiasdev.com" width="100%" height="800px" />
+          <NextcloudIframe src="https://nextcloud.matthiasdev.com/index.php/s/TbjxbKHGFJNT3sY" width="100%" height="800px" />
     </div>
     );
 }
