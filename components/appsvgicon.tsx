e0bf54b76bf2d22f753dd48e0dc48e3d8e916b25
Usama Hussain <usamahussain400@gmail.com>
2024-01-24 20:57:28 +0500

    :new: Appsvgicon component created

---
+++ components/appsvgicon.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Image from 'next/image';
+import folderIcon from '../assets/icons/folderIcon.svg'
+
+const AppSVGIcon = ({ icon, customclass }: { icon: string, customclass: string }) => {
+    const iconPath = `../assets/icons/${icon}.svg`;
+    const iconImport = require(`../assets/icons/${icon}.svg`) as string;
+
+
+    return (
+        <Image className={`${customclass} max-w-fit`} src={iconImport} alt={icon} width={24} height={24} />
+    );
+};
+
+export default AppSVGIcon;
