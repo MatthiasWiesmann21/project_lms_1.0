69e8a64b04891c24ee30edb997e97e20788ee863
MatthiasWiesmann21 <matthias@Matthiass-MacBook-Pro.local>
2024-03-28 07:44:06 +0100

    changing styling from Timer

--- app/(dashboard)/(routes)/live-event/[liveEventId]/_components/timer.tsx
+++ app/(dashboard)/(routes)/live-event/[liveEventId]/_components/timer.tsx
@@ -1,10 +1,12 @@
 const Each = ({ str, num }: { str: string; num: number }) => (
-  <div className="m-[1%] flex flex-col items-center p-[1%]">
-    <div className="m-[10px] p-[10px] text-[18px] font-extrabold">{str}</div>
-    <div className="m-[10px] mt-0 p-[10px] pt-0 text-[32px] font-black">
-      {num}
-    </div>
+  <div className="m-1 flex flex-col items-center p-1">
+  <div className="m-1 p-1 sm:text-sm md:text-lg lg:text-xl font-medium">
+    {str}
   </div>
+  <div className="m-1 p-1 text-xl sm:text-xl md:text-2xl lg:text-4xl font-black">
+    {num}
+  </div>
+</div>
 );
 
 const Timer = ({ timeRemaining, endDateTime }: any) => (
