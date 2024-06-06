0400d9cf287a3835c9b4f1bd84d4123faf4fde86
MatthiasWiesmann21 <matthiaswiesmann21@outlook.com>
2024-06-06 20:50:35 +0200

    fixing navbarRoutes in course

--- app/(course)/courses/[courseId]/_components/course-navbar.tsx
+++ app/(course)/courses/[courseId]/_components/course-navbar.tsx
@@ -3,6 +3,7 @@ import { Chapter, Course, UserProgress } from "@prisma/client";
 import { NavbarRoutes } from "@/components/navbar-routes";
 
 import { CourseMobileSidebar } from "./course-mobile-sidebar";
+import { currentProfile } from "@/lib/current-profile";
 
 interface CourseNavbarProps {
   course: Course & {
@@ -13,11 +14,16 @@ interface CourseNavbarProps {
   progressCount: number;
 }
 
-export const CourseNavbar = ({ course, progressCount }: CourseNavbarProps) => {
+export const CourseNavbar = async ({ course, progressCount }: CourseNavbarProps) => {
+  const profile = await currentProfile();
+  const profileId = profile?.id || "";
+  const profileName = profile?.name || "";
+  const profileImageUrl = profile?.imageUrl || "";
+
   return (
     <div className="flex h-full items-center border-b bg-[#ffffff] p-4 shadow-sm dark:bg-[#0A0118]">
       <CourseMobileSidebar course={course} progressCount={progressCount} />
-      <NavbarRoutes />
+      <NavbarRoutes profileId={profileId} profileName={profileName} profileImageUrl={profileImageUrl}/>
     </div>
   );
 };
