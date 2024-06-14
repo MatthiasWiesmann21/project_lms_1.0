d48026274575b31a069dfc75570a33301f4e1ed6
MatthiasWiesmann21 <matthiaswiesmann21@outlook.com>
2024-06-14 17:39:49 +0200

    fix sign-in

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -2,13 +2,9 @@ import { currentProfile } from "@/lib/current-profile";
 import { SignIn } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
  
-export default async function Page() {
-  const profile = await currentProfile();
-  const router = useRouter();
-
-  if (profile) {
-    router.push("/dashboard")
-  } else {
-    return <SignIn />;
+export default function Page() {
+    window.location.reload()
+    return (
+      <SignIn/>
+    )
   }
-}
