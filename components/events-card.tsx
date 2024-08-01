9ceac3700221fe517ed54bf3958686bd8c04553a
MatthiasWiesmann21 <matthiaswiesmann21@outlook.com>
2024-08-01 16:34:51 +0200

    updating more design adjsutments

--- components/events-card.tsx
+++ components/events-card.tsx
@@ -8,6 +8,12 @@ import { cn } from "@/lib/utils";
 import { CategoryItemCard } from "@/app/(dashboard)/(routes)/live-event/_components/category-item-card";
 import { useState } from "react";
 import { useTheme } from "next-themes";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./tooltip";
 
 interface EventsCardProps {
   id: string;
@@ -48,50 +54,64 @@ export const EventCard = ({
   };
 
   return (
-    <Link
-      href={`/live-event/${id}`}
-      className="border-2 rounded-lg transition duration-500 ease-in-out"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      style={{
-        borderColor: isHovered ? getBorderColor() : "transparent",
-      }}
-    >
-      <div className="group h-full overflow-hidden rounded-lg p-2 transition hover:shadow-sm dark:border-[#1f182b] bg-slate-100/60 dark:bg-[#0c0319]">
-        <div
-          className={cn(
-            "relative aspect-video w-full overflow-hidden rounded-md",
-            isLive && "border-2 border-rose-500"
-          )}
-        >
-          {isLive && (
-            <p className="absolute left-2 top-2 z-10 flex rounded-md bg-rose-600 p-1 text-white dark:bg-rose-600 dark:text-white">
-              Live
-              <PlayCircle className="pl-1" />
-            </p>
-          )}
-          <Image
-            fill
-            className="rounded object-cover"
-            alt={title}
-            src={imageUrl}
-          />
-        </div>
-        <div className="mt-3 p-2">
-          <div>
-            <CategoryItemCard label={category} colorCode={categoryColorCode} />
+    <TooltipProvider>
+      <Link
+        href={`/live-event/${id}`}
+        className="rounded-lg border-2 transition duration-500 ease-in-out"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        style={{
+          borderColor: isHovered ? getBorderColor() : "transparent",
+        }}
+      >
+        <div className="group h-full overflow-hidden rounded-lg bg-slate-100/60 p-2 transition hover:shadow-sm dark:border-[#1f182b] dark:bg-[#0c0319]">
+          <div
+            className={cn(
+              "relative aspect-video w-full overflow-hidden rounded-md",
+              isLive && "border-2 border-rose-500"
+            )}
+          >
+            {isLive && (
+              <p className="absolute left-2 top-2 z-10 flex rounded-md bg-rose-600 p-1 text-white dark:bg-rose-600 dark:text-white">
+                Live
+                <PlayCircle className="pl-1" />
+              </p>
+            )}
+            <Image
+              fill
+              className="rounded object-cover"
+              alt={title}
+              src={imageUrl}
+            />
           </div>
-          <div className="line-clamp-2 py-2 text-sm font-medium md:text-base">
-            {title}
+          <div className="mt-3 p-2">
+            <div>
+              <CategoryItemCard
+                label={category}
+                colorCode={categoryColorCode}
+              />
+            </div>
+            <Tooltip>
+              <TooltipTrigger>
+                <div className="text-start line-clamp-2 py-2 text-sm font-medium md:text-base">
+                  {title}
+                </div>
+              </TooltipTrigger>
+              <TooltipContent>
+                <div className="text-sm font-medium whitespace-normal">
+                  {title}
+                </div>
+              </TooltipContent>
+            </Tooltip>
+            <p className="text-[12px] text-gray-500">{`Starts: ${moment(
+              startDateTime
+            )?.format("DD-MM-YY HH:mm")}`}</p>
+            <p className="text-[12px] text-gray-500">{`Ends: ${moment(
+              endDateTime
+            )?.format("DD-MM-YY HH:mm")}`}</p>
           </div>
-          <p className="text-[12px] text-gray-500">{`Starts: ${moment(
-            startDateTime
-          )?.format("DD-MM-YY HH:mm")}`}</p>
-          <p className="text-[12px] text-gray-500">{`Ends: ${moment(
-            endDateTime
-          )?.format("DD-MM-YY HH:mm")}`}</p>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </TooltipProvider>
   );
 };
